acda6d41c3df95f7063b693aa7a642891045ddd1
Sergey Razuvaev <serg@sv-tel.ru>
2025-10-18 11:48:18 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  type: 'lib',
+
+  // Enable stylistic formatting rules
+  // stylistic: true,
+
+  // Or customize the stylistic rules
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+  },
+
+  // TypeScript and Vue are autodetected, you can also explicitly enable them:
+  typescript: true,
+})
